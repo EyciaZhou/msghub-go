52d95ea035140b4d57be37fc6904a05211d97a99
Eycia Zhou <zhou.eycia@gmail.com>
2016-06-06 21:52:01 +0800

    chan plugin

--- plugins/plugin.go
+++ plugins/plugin.go
@@ -1,73 +1,84 @@
 package plugins
 
 import (
-	"errors"
-	"fmt"
+	//"errors"
+	//"fmt"
 	"github.com/EyciaZhou/msghub.go/interface"
-	log "github.com/Sirupsen/logrus"
-	"io/ioutil"
-	"path"
+	//log "github.com/Sirupsen/logrus"
+	//"io/ioutil"
+	//"path"
 	"sync"
 	"time"
 )
 
-type LoadConf func(raw []byte) ([]GetNewer, error)
+type Plugin interface {
+	ResumeStatus(Status []byte) (PluginTask, error)
+	LoadConfig(Config interface{}) (PluginTask, error)
+	GetConfigType() interface{}
+}
 
-type GetNewer interface {
-	GetNew() (*Interface.Topic, error)
+type PluginTask interface {
+	FetchNew() (*Interface.Topic, error)
 
-	DelayBetweenCatchRound() time.Duration
+	GetDelayBetweenCatchRound() time.Duration
+
+	DumpStatus() (Status []byte)
+
+	Hash() []byte //as id, less equal 64 bytes
 }
 
 var (
 	pluginsMu       sync.Mutex
-	RegistedPlugins = make(map[string]LoadConf)
+	RegistedPlugins = make(map[string]Plugin)
 
 	pluginRunners []*pluginRunner
 )
 
-func Register(name string, fLoadConf LoadConf) {
+func Register(name string, plugin Plugin) {
 	pluginsMu.Lock()
 	defer pluginsMu.Unlock()
 
-	RegistedPlugins[name] = fLoadConf
+	RegistedPlugins[name] = plugin
 }
 
 func loadPluginConfig() error {
 	//
-	if pluginRunners != nil && len(pluginRunners) > 0 {
-		return errors.New("can't load config twice")
-	}
-
-	if len(config.ConfFileNames) != len(config.ConfPluginNames) {
-		return errors.New("the length of ConfFileNames and ConfPluginNames not same")
-	}
-
-	pluginRunners = []*pluginRunner{}
-
-	config.ConfDir = path.Clean(config.ConfDir)
-
-	log.Infof("%d plugin configs to load", len(config.ConfFileNames))
-
-	for i, fn := range config.ConfFileNames {
-		log.Infof("[%d/%d]...", i+1, len(config.ConfFileNames))
-		bs, err := ioutil.ReadFile(config.ConfDir + "/" + fn)
-		if err != nil {
-			return err
+	/*
+		if pluginRunners != nil && len(pluginRunners) > 0 {
+			return errors.New("can't load config twice")
 		}
 
-		if plugin, hv := RegistedPlugins[config.ConfPluginNames[i]]; !hv {
-			return errors.New("Doesn't registed Plugin : " + config.ConfPluginNames[i])
-		} else {
-			gns, err := plugin(bs)
+		if len(config.ConfFileNames) != len(config.ConfPluginNames) {
+			return errors.New("the length of ConfFileNames and ConfPluginNames not same")
+		}
+
+		pluginRunners = []*pluginRunner{}
+
+		config.ConfDir = path.Clean(config.ConfDir)
+
+		log.Infof("%d plugin configs to load", len(config.ConfFileNames))
+
+		for i, fn := range config.ConfFileNames {
+			log.Infof("[%d/%d]...", i+1, len(config.ConfFileNames))
+			bs, err := ioutil.ReadFile(config.ConfDir + "/" + fn)
 			if err != nil {
-				return fmt.Errorf("Error when load %d th plugin : %s", i+1, err.Error())
+				return err
 			}
-			for _, gn := range gns {
-				pluginRunners = append(pluginRunners, NewPluginRunner(gn))
+
+			if plugin, hv := RegistedPlugins[config.ConfPluginNames[i]]; !hv {
+				return errors.New("Doesn't registed Plugin : " + config.ConfPluginNames[i])
+			} else {
+				gns, err := plugin.LoadConf(bs)
+				if err != nil {
+					return fmt.Errorf("Error when load %d th plugin : %s", i+1, err.Error())
+				}
+				for _, gn := range gns {
+					pluginRunners = append(pluginRunners, NewPluginRunner(gn))
+				}
 			}
 		}
-	}
 
+		return nil
+	*/
 	return nil
 }
